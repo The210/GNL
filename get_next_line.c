32fd26979acf55541be5dceb92bc6fcb9c6a1175
Daniel HORVILLEUR <dhorvill@e1r6p11.42.fr>
2018-06-08 17:32:44 +0200

    final_version

--- get_next_line.c
+++ get_next_line.c
@@ -3,94 +3,58 @@
 /*                                                        :::      ::::::::   */
 /*   get_next_line.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ybouzgao <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: dhorvill <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2018/02/09 17:29:56 by ybouzgao          #+#    #+#             */
-/*   Updated: 2018/03/28 15:02:10 by dhorvill         ###   ########.fr       */
+/*   Created: 2018/06/03 19:21:19 by dhorvill          #+#    #+#             */
+/*   Updated: 2018/06/05 15:32:29 by dhorvill         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-char	*ft_strjoin_free(char *s1, char *s2)
+static int	give_to_line(char **line, char *extra)
 {
-	int		x;
-	int		y;
-	char	*sol;
+	char	*c;
 
-	x = s1 != NULL ? ft_strlen(s1) : 0;
-	y = s2 != NULL ? ft_strlen(s2) : 0;
-	sol = (char *)ft_memalloc(sizeof(char) * (x + y + 1));
-	x = 0;
-	while (s1 && s1[x])
-	{
-		sol[x] = s1[x];
-		x++;
-	}
-	if (s1)
-		free(s1);
-	y = 0;
-	while (s2 && s2[y])
-	{
-		sol[x + y] = s2[y];
-		y++;
-	}
-	return (sol);
-}
-
-char	*split_newline2(char *line, char **extra)
-{
-	char			*c;
-
-	if ((c = ft_strchr(line, '\n')) != NULL)
+	if ((c = ft_strchr(extra, '\n')))
 	{
 		*c = '\0';
-		*extra = ft_strdup(c + 1);
-	}
-	return (line);
-}
-
-char	*split_newline(char *line, char **extra, char *c)
-{
-//	if ((c = ft_strchr(*extra, '\n')) != NULL)
-//	{
-		*c = '\0';
-		line = ft_strdup(*extra);
-		*extra = ft_strdup(c + 1);
-//	}
-/*	else if (extra)
-	{
-		line = ft_strdup(*extra);
-		ft_strdel(extra);
-	}*/
-	return (line);
-}
-
-int	get_next_line(int fd, char **line)
-{
-	static char *extra = NULL;
-	int ret;
-	char *c;
-	char buf[BUFF_SIZE + 1];
-
-	if (line == NULL || fd < 0 || read(fd, buf, 0) == -1)
-		return (-1);
-	*line = ft_strnew(1);
-	if (extra && ((c = ft_strchr(extra, '\n')) != NULL))
-	{
-		*line = split_newline(*line, &extra, c);
+		*line = ft_strdup(extra);
+		c++;
+		ft_memmove(extra, c, ft_strlen(c) + 1);
 		return (1);
 	}
-	if (extra)
+	if (ft_strlen(extra) > 0)
 	{
 		*line = ft_strdup(extra);
-		ft_strdel(&extra);
+		*extra = '\0';
+		return (1);
 	}
-	while ((ret = read(fd, buf, BUFF_SIZE)) != 0)
+	return (0);
+}
+
+int			get_next_line(const int fd, char **line)
+{
+	static char	*extra = NULL;
+	char		buf[BUFF_SIZE + 1];
+	char		*tmp;
+	int			ret;
+
+	if (!line || fd < 0)
+		return (-1);
+	if (extra == NULL)
+		extra = ft_strnew(0);
+	while (!ft_strchr(extra, '\n'))
 	{
-		buf[ret] ='\0';
-		*line = ft_strjoin_free(*line, buf);
+		ret = read(fd, buf, BUFF_SIZE);
+		if (ret == -1)
+			return (-1);
+		if (ret == 0)
+			return (give_to_line(line, extra));
+		buf[ret] = '\0';
+		tmp = ft_strjoin(extra, buf);
+		ft_strdel(&extra);
+		extra = tmp;
 	}
-	*line = split_newline2(*line, &extra);
-	return((ft_strlen(*line) > 0) ? 1 : 0);
+	return (give_to_line(line, extra));
 }
--- get_next_line.h
+++ get_next_line.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   get_next_line.h                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ybouzgao <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: dhorvill <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2018/03/08 12:53:55 by ybouzgao          #+#    #+#             */
-/*   Updated: 2018/03/28 14:51:23 by dhorvill         ###   ########.fr       */
+/*   Created: 2018/06/05 13:08:32 by dhorvill          #+#    #+#             */
+/*   Updated: 2018/06/06 18:16:48 by dhorvill         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,9 +14,8 @@
 # define FT_GET_NEXT_LINE_H
 
 # include "libft/libft.h"
-# define BUFF_SIZE 1
+# define BUFF_SIZE 100
 # include <fcntl.h>
-# include <stdio.h>
 # include <unistd.h>
 
 int	get_next_line(int fd, char **line);
